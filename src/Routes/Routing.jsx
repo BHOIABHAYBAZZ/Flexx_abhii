8090242b1370b76f1d27a44acd3f5ef2f119e315
BHOIABHAYBAZZ <abhaybhoi917@gmail.com>
2026-07-11 18:40:09 +0530

    Fix Netlify build

--- src/Routes/Routing.jsx
+++ src/Routes/Routing.jsx
@@ -4,20 +4,12 @@ import Home from "../pages/Home";
 import Men from "../pages/Men";
 import Women from "../pages/Women";
 import Accessories from "../pages/Accessories";
-import NewArrivals from "../Pages/NewArrivals";
-
-
-
-import Footwear from "../Pages/Footwear";
-import GymEquipment from "../Pages/GymEquipment";
-import Massagers from "../Pages/Massagers";
-
-
-
-
+import NewArrivals from "../pages/NewArrivals";
+import Footwear from "../pages/Footwear";
+import GymEquipment from "../pages/GymEquipment";
+import Massagers from "../pages/Massagers";
 
 import MenuBar from "../components/Menu";
-
 import Category from "../components/Category";
 import Productdetail from "../components/Productdetail";
 import Cart from "../components/Cart";
@@ -26,46 +18,31 @@ import Success from "../components/Success";
 import Login from "../components/Login";
 import Register from "../components/Register";
 
-
 export default function Routing() {
   return (
-    <div>
+    <>
       <MenuBar />
 
       <Routes>
-       
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
 
-      
         <Route path="/new-arrivals" element={<NewArrivals />} />
         <Route path="/m" element={<Men />} />
         <Route path="/w" element={<Women />} />
-
         <Route path="/foot" element={<Footwear />} />
         <Route path="/gym" element={<GymEquipment />} />
         <Route path="/massager" element={<Massagers />} />
         <Route path="/accessorie" element={<Accessories />} />
-     
-     
 
-    
+        <Route path="/:category" element={<Category />} />
+        <Route path="/:category/:id" element={<Productdetail />} />
 
-           <Route path="/:category" element={<Category/>}/>
-        <Route path="/:category/:id" element={<Productdetail/>}/>
-       <Route path="/cart" element={<Cart />} />
-       <Route path="/login" element={<Login/>}/>
-       <Route path="/register" element={<Register/>}/>
-       
-      <Route path="/checkout" element={<Checkout/>} />
-      <Route path="/success" element={<Success/>} />
-       
-
-       
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/success" element={<Success />} />
       </Routes>
-
-
-    
-      
-    </div>
+    </>
   );
 }
